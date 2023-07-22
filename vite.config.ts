4d31198093cbc117355c3262e0f8c8dbc13fe8fe
lexi <tori@disroot.org>
2023-07-22 04:20:41 +0200

    minor refractor to fix files not being attached to tsconfig.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 // vite.config.js
+import fs from 'fs';
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config'
 import dts from 'vite-plugin-dts'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { toEntry } from './viteExporter.js';
 
 // relative to ./src/
 const exports = ["main.ts"]
@@ -18,7 +19,37 @@ export default defineConfig({
         minify: false,
     },
     plugins: [
-        dts(),
+        dts({
+            exclude: ['./vite.config.ts']
+        }),
         tsconfigPaths()
     ],
 })
+
+// helper to generate entry
+
+function toEntry({exports, main}: {
+    exports: string[],
+    main: string
+}) {
+    // update package.json
+    let pkg = JSON.parse(fs.readFileSync(resolve(__dirname, './package.json'), 'utf-8'));
+    let oldExports = pkg.exports;
+    pkg.exports = {}
+    for (const file of exports) {
+        let fileWithoutExt = file.replace(/\.[^/.]+$/, "");
+
+        pkg.exports[file === main ? '.' : ('./' + fileWithoutExt)] = {
+            import: './dist/' + fileWithoutExt + '.mjs',
+            require: './dist/' + fileWithoutExt + '.js',
+        };
+    }
+
+    if (JSON.stringify(pkg.exports) !== JSON.stringify(oldExports)) {
+        console.warn('package.json exports changed, updating...')
+        fs.writeFileSync(resolve(__dirname, './package.json'), JSON.stringify(pkg, null, 4));
+    }
+
+    return exports.map((file) => resolve(__dirname, './src/', file));
+}
+
